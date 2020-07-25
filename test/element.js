fa583c276221fefe297e6e2ba07a3928c5eb4f31
dgpreciado <37858944+dgpreciado@users.noreply.github.com>
2020-07-25 09:33:50 -0600

    added parallelization
    modded config to add a second chrome instead and added test case.

--- test/element.js
+++ test/element.js
@@ -1,8 +1,35 @@
-const { percySnapshot } = require('@percy/webdriverio');
+// const { percySnapshot } = require('@percy/webdriverio');
 
-describe("Percy test", () => {
-    it("screenshot", async ()=>{
-        await browser.url('https://www.utep.edu/');
-        await percySnapshot(browser, 'test_photo' );
+// describe("Percy test", () => {
+//     it("screenshot", async ()=>{
+//         await browser.url('https://www.utep.edu/');
+//         await percySnapshot(browser, 'test_photo' );
+//     });
+// });
+
+//run command:  npx wdio run wdio.conf.js 
+
+describe("test 1", () => {
+    it("go to www 1", ()=>{
+        browser.maximizeWindow
+        browser.url('https://www.utep.edu/');
+        console.log("test1a");
+    });
+    it("go to www 2", ()=>{
+        browser.maximizeWindow
+        browser.url('https://www.utep.edu/');
+        console.log("test1b");
+    });
+});
+describe("test 2", () => {
+    it("go to my utep 1", ()=>{
+        browser.maximizeWindow
+        browser.url('https://my.utep.edu/My');
+        console.log("test2a");
+    });
+    it("go to my utep 2", ()=>{
+        browser.maximizeWindow
+        browser.url('https://my.utep.edu/My');
+        console.log("test2b");
     });
 });
